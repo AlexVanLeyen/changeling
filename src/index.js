9e00d685c0eb06b244ceef52622166489a059a62
AlexVanLeyen <alex.vanleyen@gmail.com>
2023-08-04 14:49:11 +0800

    refactor(fancy calculator): Changed answer algorithm
    Removed add method

--- src/index.js
+++ src/index.js
@@ -2,15 +2,4 @@ console.log("hello world");
 
 let a = 1, b = 2;
 
-/**
- * @name add
- * @param {number} a 
- * @param {number} b 
- * @returns {number}
- * @deprecated this method is just extra overhead.
- */
-function add(a, b) {
-    return a + b
-}
-
-console.log("%d + %d = %d", a, b, add(a, b));
+console.log("%d + %d = %d", a, b, a + b);

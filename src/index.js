aa720e008949b782ac8dd1f405b3782eff931ee7
AlexVanLeyen <alex.vanleyen@gmail.com>
2023-08-04 14:46:44 +0800

    refactor(fancy calculator): Deprecated add method

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ console.log("hello world");
 
 let a = 1, b = 2;
 
-console.log("%d + %d = %d", a, b, a + b);
-
 /**
  * @name add
  * @param {number} a 
  * @param {number} b 
  * @returns {number}
+ * @deprecated this method is just extra overhead.
  */
 function add(a, b) {
     return a + b
 }
+
+console.log("%d + %d = %d", a, b, add(a, b));

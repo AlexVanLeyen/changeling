556ea8eedabc7f41665be0ba2c551951c2f7094d
AlexVanLeyen <alex.vanleyen@gmail.com>
2023-08-04 14:44:34 +0800

    feat(fancy calculator): Added add method

--- src/index.js
+++ src/index.js
@@ -3,3 +3,13 @@ console.log("hello world");
 let a = 1, b = 2;
 
 console.log("%d + %d = %d", a, b, a + b);
+
+/**
+ * @name add
+ * @param {number} a 
+ * @param {number} b 
+ * @returns {number}
+ */
+function add(a, b) {
+    return a + b
+}
